46530f627b48102aa228643bfc66d468e384a1f4
Jan-Otto Kröpke <mail@jkroepke.de>
2019-12-05 19:29:10 +0100

    refactor 2019.5

--- 2019/5/part2-class.js
+++ 2019/5/part2-class.js
@@ -33,7 +33,7 @@ class IntCodeComputer {
   }
 
   getParameter(numberOfParameter) {
-    const parameterMode = this.currentIntCode[numberOfParameter + 1];
+    const parameterMode = this.currentIntCode[numberOfParameter + 1] || 0;
 
     return Number(
       parameterMode === 0
@@ -68,26 +68,45 @@ class IntCodeComputer {
     return input;
   }
 
+  /*
+    Opcode 1 adds together numbers read from two positions and stores the result in a third position.
+   */
   async opcode1() {
     this.writeData(this.getParameter(1) + this.getParameter(2), 3);
     this.position += 4;
   }
 
+  /*
+    Opcode 2 works exactly like opcode 1,
+    except it multiplies the two inputs instead of adding them.
+   */
   async opcode2() {
     this.writeData(this.getParameter(1) * this.getParameter(2), 3);
     this.position += 4;
   }
 
+  /*
+    Opcode 3 takes a single integer as input and saves it to the position given by its only parameter.
+   */
   async opcode3() {
     this.writeData(await this.getConsoleInput(), 1);
     this.position += 2;
   }
 
+  /*
+    Opcode 4 outputs the value of its only parameter.
+   */
   async opcode4() {
     this.output += this.getParameter(1).toString();
     this.position += 2;
   }
 
+  /*
+  Opcode 5 is jump-if-true:
+    if the first parameter is non-zero,
+    it sets the instruction pointer to the value from the second parameter.
+    Otherwise, it does nothing.
+  */
   async opcode5() {
     if (this.getParameter(1) !== 0) {
       this.position += this.getParameter(2);
@@ -97,6 +116,12 @@ class IntCodeComputer {
     }
   }
 
+  /*
+  Opcode 6 is jump-if-false:
+    if the first parameter is zero,
+    it sets the instruction pointer to the value from the second parameter.
+    Otherwise, it does nothing.
+  */
   async opcode6() {
     if (this.getParameter(1) === 0) {
       this.position += this.getParameter(2);
@@ -106,6 +131,12 @@ class IntCodeComputer {
     }
   }
 
+  /*
+  Opcode 7 is less than:
+    if the first parameter is less than the second parameter,
+    it stores 1 in the position given by the third parameter.
+    Otherwise, it stores 0.
+   */
   async opcode7() {
     this.writeData(
       Number(this.getParameter(1) < this.getParameter(2)),
@@ -114,6 +145,12 @@ class IntCodeComputer {
     this.position += 4;
   }
 
+  /*
+  Opcode 8 is equals:
+    if the first parameter is equal to the second parameter,
+    it stores 1 in the position given by the third parameter.
+    Otherwise, it stores 0.
+   */
   async opcode8() {
     this.writeData(
       Number(this.getParameter(1) === this.getParameter(2)),
