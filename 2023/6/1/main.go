65802f27cbaaa9a40432306a79dd2b2a81302d81
Jan-Otto Kröpke <mail@jkroepke.de>
2023-12-11 22:14:59 +0100

    2023/6/1

--- 2023/6/1/main.go
+++ 2023/6/1/main.go
@@ -1,1 +1,53 @@
 package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+)
+
+type Race struct {
+	time        int
+	distance    int
+	buttonTimes []int
+}
+
+func main() {
+	fmt.Println(day6())
+}
+
+func day6() any {
+	bytes, err := os.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	var races []Race
+
+	lines := strings.Split(string(bytes), "\n")
+	matches := regexp.MustCompile(`(\d+)`).FindAllString(lines[0], -1)
+	for _, match := range matches {
+		races = append(races, Race{time: StringToInt(match)})
+	}
+	matches = regexp.MustCompile(`(\d+)`).FindAllString(lines[1], -1)
+	for i, match := range matches {
+		races[i].distance = StringToInt(match)
+	}
+
+	var distance int
+	result := 1
+
+	for i, race := range races {
+		for buttonTime := 1; buttonTime <= race.time; buttonTime++ {
+			distance = (race.time - buttonTime) * (buttonTime)
+			if distance > race.distance {
+				races[i].buttonTimes = append(races[i].buttonTimes, buttonTime)
+			}
+		}
+		fmt.Println(races[i].buttonTimes)
+		result *= len(races[i].buttonTimes)
+	}
+
+	return result
+}
